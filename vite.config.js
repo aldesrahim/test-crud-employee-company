c76eac78340016d1b47a9a65f40ddc7747a9a554
aldesrahim <aldesrahim@gmail.com>
2024-12-21 09:16:15 +0700

    feat: install ant-design-vue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import components from "unplugin-vue-components/vite"
+import {AntDesignVueResolver} from "unplugin-vue-components/resolvers"
 
 export default defineConfig({
     plugins: [
@@ -17,5 +19,12 @@ export default defineConfig({
                 },
             },
         }),
+        components({
+            resolvers: [
+                AntDesignVueResolver({
+                    importStyle: false
+                })
+            ]
+        })
     ],
 });
